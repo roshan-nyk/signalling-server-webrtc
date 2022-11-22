b2a91714913bf91c99910dba60e24afa695a488a
Roshan Kumar Nayak <nayak.803@gmail.com>
2022-11-23 01:39:23 +0530

    testing15

--- index.js
+++ index.js
@@ -171,10 +171,10 @@ webSocket.on("request", (req) => {
   });
 });
 
-// app.use(express.static(path.join(__dirname, "./public")));
-// app.get("/", (req, res) => {
-//   // res.sendFile(path.join(__dirname, "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "./public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`);
 // });
